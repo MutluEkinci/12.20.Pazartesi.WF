a407cc9915d0672a7f89b7c2687538152789ee5c
MqNC8 <MqNC8@GODOFGAME-V2>
2021-12-22 00:52:39 +0300

    Add project files.

---
+++ 12.20.Pazartesi/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace _12._20.Pazartesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnSec = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btndurdur = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(289, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 377);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnSec
+            // 
+            this.BtnSec.Location = new System.Drawing.Point(289, 438);
+            this.BtnSec.Name = "BtnSec";
+            this.BtnSec.Size = new System.Drawing.Size(172, 84);
+            this.BtnSec.TabIndex = 1;
+            this.BtnSec.Text = "DosyaSeç";
+            this.BtnSec.UseVisualStyleBackColor = true;
+            this.BtnSec.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(690, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // Btndurdur
+            // 
+            this.Btndurdur.Location = new System.Drawing.Point(488, 438);
+            this.Btndurdur.Name = "Btndurdur";
+            this.Btndurdur.Size = new System.Drawing.Size(172, 84);
+            this.Btndurdur.TabIndex = 3;
+            this.Btndurdur.Text = "Durdur";
+            this.Btndurdur.UseVisualStyleBackColor = true;
+            this.Btndurdur.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1408, 576);
+            this.Controls.Add(this.Btndurdur);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnSec);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnSec;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btndurdur;
+    }
+}
+
---
+++ 12.20.Pazartesi/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12._20.Pazartesi
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //string[] dosyalar;
+        List<string> dosyalar;
+        int sayac = 0;
+        int ToplamResim;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dosyalar = new List<string>();
+            folderBrowserDialog1.ShowDialog();
+            //dosyalar = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            foreach (string dosya in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+            {
+                FileInfo file = new FileInfo(dosya);
+                if (file.Extension == ".jpg" || file.Extension == ".JPEG")
+                    dosyalar.Add(dosya);
+            }
+            timer1.Enabled = true;
+            ToplamResim = dosyalar.Count;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = dosyalar[sayac];
+            label1.Text = (sayac + 1) + "/" + ToplamResim;
+            sayac++;
+            if (sayac >= ToplamResim)
+            {
+                sayac = 0;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
